474c760406e5d484946e75ef4ce0bebad4e3262a
bo.wu <wbbjtu@163.com>
2022-11-21 21:56:19 -0500

    add a databuff in main.c

--- main.c
+++ main.c
@@ -3,10 +3,15 @@
 int main(void)
 {
   int i = 0, j = 0;
-
+  char databuff[128];
+  
   printf("hello github!\n");
   printf("add a new branch");  
 
+  for(i = 0, i < sizeof(databuff), i++)
+  {
+      databuff[i] = i;
+  }
 
   return 0;
 }
